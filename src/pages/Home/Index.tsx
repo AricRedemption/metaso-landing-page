6287378559aa15a96879a2577cb4b2883c6b0ff1
AricRedemption <aricredemption@gmail.com>
2024-11-11 11:05:56 +0800

    feat: restore previously commented image imports and update tsconfig for path aliasing

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -5,10 +5,10 @@ import Earth from "@/assets/images/earth.png";
 import Money from "@/assets/images/money.png";
 import Person from "@/assets/images/person.png";
 import Logo from "@/assets/images/logo.svg?react";
-// import Intro from "@/assets/images/icons/intro.png";
-// import Image1 from "@/assets/images/icons/image1.png";
-// import Image2 from "@/assets/images/icons/image2.png";
-// import Image3 from "@/assets/images/icons/image3.png";
+import Intro from "@/assets/images/icons/intro.png";
+import Image1 from "@/assets/images/icons/image1.png";
+import Image2 from "@/assets/images/icons/image2.png";
+import Image3 from "@/assets/images/icons/image3.png";
 import Icon1 from "@/assets/images/icons/icon1.svg?react";
 import Icon2 from "@/assets/images/icons/icon2.svg?react";
 import Icon3 from "@/assets/images/icons/icon3.svg?react";
@@ -136,7 +136,7 @@ const Home: React.FC = () => {
                 </p>
               </div>
             </div>
-            {/* <img src={Intro} alt="Intro" className="w-1/2 mx-auto" /> */}
+            <img src={Intro} alt="Intro" className="w-1/2 mx-auto" />
           </div>
         </div>
       </div>
@@ -161,7 +161,7 @@ const Home: React.FC = () => {
               <h1 className="text-lg sm:text-xl xl:text-2xl font-bold text-[#002E33] mt-3.5">
                 Download & Install MetaSo Node
               </h1>
-              {/* <img src={Image1} alt="" className="ml-auto size-[125px]" /> */}
+              <img src={Image1} alt="" className="ml-auto size-[125px]" />
             </div>
             <div className="flex flex-col bg-white/[0.8] px-8 pt-[30px] w-full h-[300px] rounded-2xl col-span-3 md:col-span-1">
               <div className="bg-white rounded-full size-12 p-2">
@@ -178,7 +178,7 @@ const Home: React.FC = () => {
               <h1 className="text-lg sm:text-xl xl:text-2xl font-bold text-[#002E33] mt-3.5">
                 Config for Your Own Social Media App
               </h1>
-              {/* <img src={Image2} alt="" className="ml-auto size-[125px]" /> */}
+              <img src={Image2} alt="" className="ml-auto size-[125px]" />
             </div>
             <div className="flex flex-col bg-white/[0.8] px-8 pt-[30px] w-full h-[300px] rounded-2xl col-span-3 md:col-span-1">
               <div className="bg-white rounded-full size-12 p-2">
@@ -195,7 +195,7 @@ const Home: React.FC = () => {
               <h1 className="text-lg sm:text-xl xl:text-2xl font-bold text-[#002E33] mt-3.5">
                 Promote It And Get $METASO
               </h1>
-              {/* <img src={Image3} alt="" className="ml-auto size-[125px]" /> */}
+              <img src={Image3} alt="" className="ml-auto size-[125px]" />
             </div>
           </div>
         </div>
